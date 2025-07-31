f3b0268d7c610687f26f36c40baf3237482c1ad1
Kathirr007 <kathirr007@gmail.com>
2025-07-31 12:10:44 +0530

    updated lint-staged config

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,14 +1,14 @@
 import micromatch from 'micromatch'
 
 export default {
-  'client/**/*.{js,ts,vue,json}': (files) => {
+  'client/**/*.{js, mjs, ts, mts, vue, json}': (files) => {
     // Exclude specific files from linting
     // This is useful for files that are auto-generated or not meant to be linted
     // For example, you might want to exclude 'typed-router.d.ts', 'components.d.ts', and 'auto-imports.d.ts'
     const match = micromatch.not(files, 'shims.d.ts', 'typed-router.d.ts', 'components.d.ts', 'auto-imports.d.ts')
     return `eslint --config client/eslint.config.js --no-warn-ignored --cache --fix ${match.join(' ')}`
   },
-  '*.{js,ts,json}': (files) => {
+  '*.{js, mjs, ts, mts, json}': (files) => {
     // Exclude specific files from linting
     // This is useful for files that are auto-generated or not meant to be linted
     // For example, you might want to exclude 'typed-router.d.ts', 'components.d.ts', and 'auto-imports.d.ts'
